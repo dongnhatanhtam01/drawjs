d46ad9586c8548008f9db863898eca4f67689793
TongXin <67026642+dongnhatanhtam01@users.noreply.github.com>
2022-07-06 03:12:37 +0700

    remove garbage

--- component/component.js
+++ component/component.js
@@ -1,15 +1,3 @@
-// class AppDrawer extends HTMLElement {}
-// window.customElements.define('app-drawer', AppDrawer)
-
-// // Or use an anonymous class if you don't want a named constructor in current scope.
-// // window.customElements.define('app-drawer', class extends HTMLElement {})
-
-// window.customElements.define('x-foo-with-markup', class extends HTMLElement {
-//     connectedCallback() {
-//     this.innerHTML = "<b>I'm an x-foo-with-markup!</b>";
-//     }
-// });
-
 class MyElement extends HTMLElement {
     constructor() {
         super();
